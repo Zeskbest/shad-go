6a981e7db433780be379a71a7d2223c7e04d3bb0
Time <tim-ef@ya.ru>
2024-12-21 17:55:30 +0700

    urlfetch

--- urlfetch/main.go
+++ urlfetch/main.go
@@ -2,6 +2,29 @@
 
 package main
 
-func main() {
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
 
+func check(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func fetchAndPrint(url string) {
+	resp, err := http.Get(url)
+	check(err)
+	buf, err := io.ReadAll(resp.Body)
+	check(err)
+	fmt.Println(string(buf))
+}
+
+func main() {
+	for _, url := range os.Args[1:] {
+		fetchAndPrint(url)
+	}
 }
