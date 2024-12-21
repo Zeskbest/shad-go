ffbe852c7af77201afdd04c653b491f8861942e9
Time <tim-ef@ya.ru>
2024-12-21 17:09:43 +0700

    wordcount

--- wordcount/main.go
+++ wordcount/main.go
@@ -2,6 +2,36 @@
 
 package main
 
-func main() {
+import (
+	"fmt"
+	"io"
+	"os"
+	"strings"
+)
 
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func main() {
+	res := make(map[string]int)
+	for _, f := range os.Args[1:] {
+		file, err := os.Open(f)
+		check(err)
+
+		buf, err := io.ReadAll(file)
+		check(err)
+		check(file.Close())
+
+		for _, line := range strings.Split(string(buf), "\n") {
+			res[line] += 1
+		}
+	}
+	for s, i := range res {
+		if i > 1 {
+			fmt.Printf("%d\t%s\n", i, s)
+		}
+	}
 }
